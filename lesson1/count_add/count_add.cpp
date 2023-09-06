26618ac379dd88df9ef702e4eed3648b1b05cd26
Emily Wang <esw77@cornell.edu>
2023-09-06 18:04:43 -0400

    CMake setup for lesson1 cpp json linking

--- lesson1/count_add/count_add.cpp
+++ lesson1/count_add/count_add.cpp
@@ -1,14 +1,12 @@
-#include <iostream>
 #include <fstream>
-#include "json.hpp"
+#include <iostream>
+#include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   // Check if the filename is provided as a command-line argument
-  if (argc < 2)
-  {
+  if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
     return 1;
   }
@@ -18,7 +16,8 @@ int main(int argc, char *argv[])
 
   // Read the JSON file into a std::string
   std::ifstream ifs(filename);
-  std::string json_str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+  std::string json_str((std::istreambuf_iterator<char>(ifs)),
+                       std::istreambuf_iterator<char>());
 
   // Parse the JSON string into a json object
   auto j = json::parse(json_str);
@@ -27,14 +26,11 @@ int main(int argc, char *argv[])
   int add_count = 0;
 
   // Loop through all functions
-  for (const auto &func : j["functions"])
-  {
+  for (const auto &func : j["functions"]) {
     // Loop through all instructions in each function
-    for (const auto &instr : func["instrs"])
-    {
+    for (const auto &instr : func["instrs"]) {
       // Check if the operation is 'add'
-      if (instr.find("op") != instr.end() && instr["op"] == "add")
-      {
+      if (instr.find("op") != instr.end() && instr["op"] == "add") {
         add_count++;
       }
     }
