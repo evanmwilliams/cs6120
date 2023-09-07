ce96a2d29feac10818ca16d900e424fb860f2965
Evan Williams <emw236@cornell.edu>
2023-09-07 13:15:20 -0400

    printing

--- lesson3/lvn.cpp
+++ lesson3/lvn.cpp
@@ -12,17 +12,24 @@ using instruction = json;
 
 json make_lvn_val(json instr,
                   std::vector<std::pair<json, std::string>> const &num2val,
-                  std::unordered_map<std::string, int> const &var2num) {
+                  std::unordered_map<std::string, int> const &var2num)
+{
   std::string op = instr["op"];
   json canonical_val;
-  if (op == "jmp" || op == "br") {
+  if (op == "jmp" || op == "br")
+  {
     // do nothing
-  } else if (op == "call") {
+  }
+  else if (op == "call")
+  {
     // do nothing
-  } else {
+  }
+  else
+  {
     canonical_val["op"] = op;
     std::vector<int> canonical_args;
-    for (json const &arg : instr["args"]) {
+    for (json const &arg : instr["args"])
+    {
       canonical_args.push_back(var2num.at(arg.dump()));
     }
     sort(canonical_args.begin(), canonical_args.end());
@@ -32,40 +39,78 @@ json make_lvn_val(json instr,
 }
 
 int find_val(json instr_as_lvn_val,
-             std::vector<std::pair<json, std::string>> const &num2val) {
-  for (int i = 0; i < num2val.size(); i++) {
+             std::vector<std::pair<json, std::string>> const &num2val)
+{
+  for (int i = 0; i < num2val.size(); i++)
+  {
     auto &[cand_val, canonical_var] = num2val[i];
-    if (cand_val == instr_as_lvn_val) {
+    if (cand_val == instr_as_lvn_val)
+    {
       return i;
     }
   }
-  return -1;
+  return -1; // not found
 }
 
-void lvn(json const &program) {
+void lvn(json const &program)
+{
   std::vector<std::vector<instruction>> basic_blocks = find_blocks(program);
 
-  for (auto block : basic_blocks) {
+  for (auto block : basic_blocks)
+  {
     std::unordered_map<std::string, int> var2num;      // x -> 0
     std::vector<std::pair<json, std::string>> num2val; // idx: 0, ({const 4}, x)
-    for (auto instr : block) {
+    for (auto instr : block)
+    {
       json lvn_ify_instr = make_lvn_val(instr, num2val, var2num);
-      if (lvn_ify_instr.empty()) {
+      if (lvn_ify_instr.empty())
+      {
         continue;
       }
 
       int value_number = find_val(lvn_ify_instr, num2val);
 
-      if (value_number == -1) {
+      // value not in the table
+      if (value_number == -1)
+      {
         // give this instr a fresh value number
         num2val.push_back({lvn_ify_instr, instr["dest"]});
-        var2num[instr["dest"]] = num2val.size();
-      } else {
+        var2num[instr["dest"]] = num2val.size() - 1;
+      }
+      // value in the table
+      else
+      {
         // replace instr with found val
         // point var2num[dest] = value_number
+        var2num[instr["dest"]] = value_number;
+        auto found_val = num2val[value_number];
+        instr = found_val.first; // is this right?
       }
     }
   }
 }
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[])
+{
+  // Check if the filename is provided as a command-line argument
+  if (argc < 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+    return 1;
+  }
+
+  // Read the filename from command-line argument
+  std::string filename = argv[1];
+
+  // Read the JSON file into a std::string
+  std::ifstream ifs(filename);
+  std::string json_str((std::istreambuf_iterator<char>(ifs)),
+                       std::istreambuf_iterator<char>());
+
+  // Parse the JSON string into a json object
+  auto j = json::parse(json_str);
+
+  lvn(j);
+
+  std::cout << j << std::endl;
+}
